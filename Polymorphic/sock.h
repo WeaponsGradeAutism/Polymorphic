39fea92b7af035e1fdfb8d2942927c787932e15d
WeaponsGradeAutism <WeaponsGradeAutism@gmail.com>
2017-02-04 15:01:08 -0800

    fix method signature

--- Polymorphic/sock.h
+++ Polymorphic/sock.h
@@ -32,7 +32,7 @@ void* getConnectionFromPeerID(uint16_t peerID);
 void* getConnectionFromServiceID(uint16_t serviceID);
 POLYM_CONNECTION_INFO* getInfoFromConnection(void *connection);
 POLYM_CONNECTION_INFO* getServiceConnectionInfo(int index);
-int getCurrentServiceConnections(void** OUT_connectionArray, uint32_t maxCount);
+int getCurrentServiceConnections(void** OUT_connectionArray, unsigned int maxCount);
 int getCurrentPeerConnections(void** OUT_connectionArray, uint32_t maxCount);
 
 int addressConnected(char *stringAddress, uint16_t port, uint8_t protocol);
